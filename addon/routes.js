792347054a90317dcf4f5db71aa6e0184263cb57
Nóra Szepes <nora.szepes@tenforce.com>
2017-09-27 15:05:58 +0200

    cleaned router

--- addon/routes.js
+++ addon/routes.js
@@ -1,9 +1,6 @@
 import buildRoutes from 'ember-engines/routes';
 
 export default buildRoutes(function() {
-  this.route('test');
-
-  this.route('compact');
   this.route('editor', function() {
     this.route('edit', {
       path: ':docker_compose_id'
